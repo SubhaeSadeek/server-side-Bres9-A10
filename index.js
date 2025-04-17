48e3d4eccb9d32bd2629b8455d8f4ccc5e32fc97
SubhaeSadeek <nurun.nabi@outlook.com>
2025-04-17 19:56:10 +0600

    made monodb uri, created mongodb client and successfully pinged mongodb

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// including MiddleWare
+app.use(cors);
+app.use(express.json());
 
+// connection config for mongodb
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.i6qlf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const database = client.db('ChillGamerDB');
+       /*  const coffeeCollection = database.collection('coffeeDetails');
+        const userCollection = database.collection('users'); */
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) =>{
     res.send("Alhamdulillah, made this server for Assignment 10");
